1ba53d654e0e9eeed53bb5d72e7302110f6467f3
Jayson Baloyi <jysnbly7@gmail.com>
2025-05-02 15:24:39 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import VideoCarousel from './components/VideoCarousel'
+
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <VideoCarousel />
+
+    </main>
+  )
+}
+
+export default App
